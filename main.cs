7b909e08f6f8b59a23420e22c41263a320ff889e
20074470 <20074470@mydbs.ie>
2026-01-15 20:09:24 +0000

    exercise solution draft

--- main.cs
+++ main.cs
@@ -1,1 +1,32 @@
-Console.WriteLine("Hello World Again!");
+//Console.WriteLine("Hello World Again!");
+using system;
+
+class SumProgram
+{
+    static in Sum(int a, int b, int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            if (i% a == 0 || i% b == 0)
+            {
+                total[] += i;
+            }
+            return total;
+        }
+    }
+
+
+    static void Main()
+    {
+        int a = 3;
+        int b = 5;
+        int n = 10;
+
+        int sumTotal = Sum(int a, int b, int n);
+    }
+
+        
+
+
+
+}
